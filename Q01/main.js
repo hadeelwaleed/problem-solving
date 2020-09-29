76c681684990c71ded3dc563f5fbde4efe526fb4
Mohammad-Jouza <m.jozaa@yahoo.com>
2020-09-29 19:39:20 +0300

    change q 1

--- Q01/main.js
+++ Q01/main.js
@@ -3,7 +3,7 @@ console.log('Problem Solving Q: 1 ');
 /* Longest Word */
 
 /*  
-Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length.
 */
 
 function LongestWord(sen) {
