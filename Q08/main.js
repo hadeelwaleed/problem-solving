ad875e34fdcf59288af0820589ea302088783a6f
Mohammad-Jouza <m.jozaa@yahoo.com>
2020-11-15 00:07:45 +0200

    fix question 8

--- Q08/main.js
+++ Q08/main.js
@@ -12,8 +12,11 @@ function uniqueItems() {
 
 /* 
 Examples:
+uniqueItems([1, 1, 1, 1, 1]) // => 1
+uniqueItems([3, 3, 5, 3]) // => 2
+uniqueItems([5, 1, 1, 5, 1]) // => 2
+uniqueItems([1, 6, 7]) // => 3
 uniqueItems([1, 2, 3, 1, 2, 3]) // => 3
-uniqueItems([3, 3, 5, 3]) // => 1
+uniqueItems([5, 1, 2, 2, 1, 5]) // => 3
 uniqueItems([5, 77, 1, 3]) // => 4
-uniqueItems([5, 1, 2, 2, 1, 5]) // => 0
 */
