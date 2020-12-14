4639fe2302823499d5a7233abaf80044f56d4905
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-12-14 22:12:11 +0200

    tree data stracture

---
+++ tree/tree.js
@@ -0,0 +1,24 @@
+console.log('tree data structure ');
+
+/* Longest Word */
+
+/*  
+implement the tree data structure:
+A tree is a data structure where a node can have zero or more children. Each node contains a value. 
+, the connection between nodes is called edges. 
+*/
+
+class Tree {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+  // add new cheldren to the tree
+  addChildren(value) {}
+
+  // check if the value is exist in the tree
+  isContain(value) {}
+
+}
+
+let tree1 = new Tree(5);
