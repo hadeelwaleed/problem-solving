106d79cf2a18297321520c5679525967e848c25d
hadeelwaleed <h.w.alkharouf.gmail.com>
2020-12-27 00:45:16 +0200

    p5

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+function maxChar(str) {
+    // we use map or forloop to serch on the string about common char
+//     obj = []
+// obj.map(function(currentValue, index, arr), thisValue)
+// const word =[];
+// for (i = 0;; i++) {
+
+// }
+}
 
 module.exports = maxChar;
